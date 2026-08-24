cb33baeed1defc8e8e2dc95ce675b289f83071e7
zaman ehsani <zamanehsani@gmail.com>
2026-08-24 14:54:43 +0400

    removed the border on the mobile header

--- app/components/Header/MobileHeader.tsx
+++ app/components/Header/MobileHeader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function MobileHeader() {
   return (
-    <header className="flex h-20 items-center justify-center border-b border-slate-100 bg-white px-4 py-3">
+    <header className="flex h-20 items-center justify-center bg-white px-4 py-3">
       <Link href="/home" className="flex items-center justify-center gap-3">
         <div className="flex h-20 w-20 items-center justify-center overflow-hidden rounded-full">
           <Image
